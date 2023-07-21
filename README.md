825614ef02f7a9620148aef9fe19962cd5ec49ca
emmanuel <blessedemmachukwu2020@gmail.com>
2023-07-21 09:05:35 +0200

    second commit

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-1. we are going to have a contract controlled by a DOA
 
-2. Every every traction that the DAO wants to send has to be voted on 
+# DOA PROJECT
 
-3. We will use ERC20 token for voting (Bad model, please reserch better models)
+# 1. we are going to have a contract controlled by a DOA
+
+# 2. Every every traction that the DAO wants to send has to be voted on 
+
+# 3. We will use ERC20 token for voting (Bad model, please reserch better models)
--- src/Box.sol
+++ src/Box.sol
@@ -1,3 +1,20 @@
 // contracts/Box.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private s_number;
+
+    event NumberChange(uint256 number);
+
+    function store(uint256 newNumber) public onlyOwner {
+        s_number = newNumber;
+        emit NumberChange(newNumber);
+    }
+
+    function getNumber() external view returns (uint256) {
+        return s_number;
+    }
+}
